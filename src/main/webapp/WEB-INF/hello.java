74a0caa5227b14e4090c02f98198ff907332d2be
jafarRED <106249984+jafarRED@users.noreply.github.com>
2022-06-23 01:23:49 +0530

    newww cmt

--- src/main/webapp/WEB-INF/hello.java
+++ src/main/webapp/WEB-INF/hello.java
@@ -1,5 +1,5 @@
 class hello{  
     public static void main(String args[]){  
-     System.out.println("Hellooooooo new data Java");  
+     System.out.println("Hellooooooo new okkkkkka data Java");  
     }  
 }  
